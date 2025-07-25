0b01066bdb10990b76ac2a3a20465dde93cb8b31
Aditya Kumar <aditya202666@gmail.com>
2025-07-25 10:40:33 +0530

    fix: correct error logging in production and ensure proper status code handling

--- src/middlewares/apiErrorHandler.js
+++ src/middlewares/apiErrorHandler.js
@@ -3,10 +3,10 @@ const errorHandler = (err, req, res, next) => {
   if (process.env.NODE_ENV === "development") {
     // console.info("Error Stack:", err.stack);
   } else {
-    console.info("Error Message:", err.message);
+    console.info("Error Message:", err);
   }
 
-  res.status(err.statusCode).json({success:err.success, message:err.message})
+  res.status(int(err.statusCode)).json({success:err.success, message:err.message})
   
 };
 export default errorHandler ;
